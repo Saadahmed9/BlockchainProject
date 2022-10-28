484b8cbcdf0c9b4e6cf4057cb9ea606c35b8fd2f
Sai Kiran Janga <jangasaikiran@gmail.com>
2022-10-28 11:55:38 -0400

    Updated campaigns.html

--- donation-app/index.js
+++ donation-app/index.js
@@ -1,30 +1,51 @@
 var express = require('express');
 var engines = require('consolidate');
 var path = require('path');
+var mysql = require('mysql');
 
 var app = express();
 
 app.set('views', __dirname + '/src/views');
-app.engine('html', engines.mustache);
+app.engine('html', engines.ejs);
 app.set('view engine', 'html');
 
-console.log("Printing!");
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "donations"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
 
 // data = mysql data
 
-app.use(express.static('src/views'));
+// app.use(express.static('src'));
 // app.use(express.static('../ballot-contract/build/contracts'));
 app.get('/', function (req, res) {
-  res.render('index.html', data);
+  // con.query('select * from campaigns', function (err, result) {
+  //   res.render('index.html', {sample: 'sample'});
+  // });
+  res.render('hello.html', {name: 'Sai Kiran'});
 });
 app.get('/campaigns', function (req, res) {
-  res.render('index.html');
+  con.query('select * from campaigns where status="OPEN"', function (err, result) {
+    res.render('campaigns.html', {result: result});
+  });
 });
 app.get('/campaigns/created', function (req, res) {
-  res.render('campaigns_created.html');
+  con.query('select * from campaigns where status="OPEN"', function (err, result) {
+    res.render('campaigns_created.html', {result: result});
+  });
 });
 app.get('/campaigns/donated', function (req, res) {
-  res.render('campaigns_donated.html');
+  con.query('select * from campaigns where status="OPEN"', function (err, result) {
+    res.render('campaigns_donated.html', {result: result});
+  });
 });
 
 app.listen(3000, function () {
