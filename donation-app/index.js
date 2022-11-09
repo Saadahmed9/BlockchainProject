0f7997a4467babed3748d9284d1d9a82e14352b0
Saadahmed9 <93593538+Saadahmed9@users.noreply.github.com>
2022-11-08 23:17:48 -0500

    Add files via upload

---
+++ donation-app/index.js
@@ -0,0 +1,93 @@
+var express = require('express');
+var engines = require('consolidate');
+var path = require('path');
+var mysql = require('mysql');
+// const Web3 = require('web3');
+// const { callbackify } = require('util');
+
+// console.log(Web3.givenProvider);
+// var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
+
+// console.log(web3.eth.accounts[0]);
+var app = express();
+
+app.set('views', __dirname + '/src');
+app.engine('html', engines.ejs);
+app.set('view engine', 'html');
+app.use(express.json());
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "donations"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
+// data = mysql data
+
+app.use(express.static('src'));
+app.use(express.static('../donation-contract/build/contracts'));
+app.get('/', function (req, res) {
+  // con.query('select * from campaigns', function (err, result) {
+  //   res.render('index.html', {sample: 'sample'});
+  // });
+  res.render('hello.html', {name: 'Sai Kiran'});
+});
+
+app.get('/campaigns/open', function (req, res) {
+  // return "Hel";
+  con.query('select * from campaigns where status="OPEN"', function (err, result) {
+    res.json(result);
+  });
+});
+
+app.get('/campaigns', function (req, res) {
+  con.query('select * from campaigns where status="OPEN"', function (err, result) {
+    res.render('campaigns.html', {result: result});
+  });
+});
+
+app.get('/campaigns/created', function (req, res) {
+  con.query('select * from campaigns where status="OPEN"', function (err, result) {
+    res.render('campaigns_created.html', {result: result});
+  });
+});
+
+app.post('/campaigns/add', function (req, res) {
+  // console.log(req);
+  con.query(`INSERT INTO campaigns (id, created_by, status, vendor, description, target, deposit, amount_raised) VALUES ('8','1','OPEN','1','${req.body['description']}', '${req.body['amountRequired']}','${req.body['deposit']}','0')`);
+  
+  res.sendStatus(200);
+});
+
+app.get('/campaigns/donated', function (req, res) {
+  con.query('select * from campaigns where status="OPEN"', function (err, result) {
+    res.render('campaigns_donated.html', {result: result});
+  });
+});
+
+
+app.post('/donations/add', function (req, res) {
+  con.query(`INSERT INTO donations (campaign_id, donated_by, amount) VALUES (${req.body['campaignId']}, '${req.body['donatedBy']}', ${req.body['amount']})`);
+  con.query(`UPDATE campaigns SET amount_raised = amount_raised +  ${req.body['amount']} where id=${req.body['campaignId']}`);
+  res.sendStatus(200);
+});
+
+app.get('/donations', function (req, res) {
+  con.query(`SELECT * from donations where campaign_id=${req.query['campaignId']} order by created_on`, function (err, result) {
+    res.json(result);
+  });
+});
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
