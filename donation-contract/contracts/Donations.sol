b0fa38abe94fba5b7fff0bb539174bbd5405b0ce
Saadahmed9 <93593538+Saadahmed9@users.noreply.github.com>
2022-12-03 19:51:30 -0500

    Add files via upload

--- donation-contract/contracts/Donations.sol
+++ donation-contract/contracts/Donations.sol
@@ -4,6 +4,17 @@ pragma solidity 0.8.17;
 contract Donations {
     
     enum Status {OPEN, CLOSED, EXPIRED}
+    string public constant name = "MyToken";
+    string public constant symbol = "DTC";
+    uint8 public constant decimals = 0;  
+
+    mapping(address => uint256) balances;
+
+    mapping(address => mapping (address => uint256)) allowed;
+    
+    uint256 totalSupply_;
+
+    using SafeMath for uint256;
 
     struct Campaign {
         uint id;
@@ -24,15 +35,23 @@ contract Donations {
     mapping(uint => Campaign) public campaigns;
     uint public counter;
     uint public percentToDeposit;
-    
-    constructor (uint percentToDepositValue) {
+    //address receiver;
+    constructor (uint percentToDepositValue,uint256 total) {
         counter = 0;
         percentToDeposit = percentToDepositValue;
+        totalSupply_ = total;
+	    balances[msg.sender] = totalSupply_;
     }  
 
-    event Donation(uint campaignId, address _from, uint _value);
+    //event Donation(uint campaignId, address _from, uint _value);
     event Transition(uint campaignId, Status status);
     event CreateCampaign(uint campaignId, uint target, uint deposit, address createdBy, address vendor);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    event Donation(uint campaignId,address from, uint numTokens);
+    event TransferToken(address to_);
+    event GetBalance(uint numTokens);
+    //event Transfer(address indexed from, address indexed to, uint tokens);
+    
 
     modifier validTransition(uint campaignId, Status status) {
         require(campaigns[campaignId].status == status);
@@ -59,6 +78,64 @@ contract Donations {
         _;
     }
 
+    //ERC20 functions
+     function totalSupply() public view returns (uint256) {
+	return totalSupply_;
+    }
+    
+    function DtcBalance() public {
+        emit GetBalance(balances[msg.sender]);
+        //return balances[account];
+    }
+
+    function donate(uint campaignId, uint numTokens) public payable {
+        require(numTokens <= balances[msg.sender]);
+        Campaign storage campaign = campaigns[campaignId];
+        campaign.fundsRaised += numTokens;
+        balances[msg.sender] = balances[msg.sender].sub(numTokens);
+        campaign.donationsHash = uint(keccak256(abi.encodePacked(campaign.donationsHash,numTokens,msg.sender)));
+        emit Donation(campaignId,msg.sender, numTokens);
+        if (campaign.fundsRaised == campaign.target){
+            campaign.vendor.transfer(campaign.target);
+            payable(campaign.createdBy).transfer(campaign.deposit);
+            closeCampaign(campaign,Status.CLOSED);
+        }
+    }
+
+    function transfertoken() public payable {
+        uint numTokens=20;
+        require(numTokens >= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender].add(numTokens);
+        totalSupply_=totalSupply_-numTokens;
+        emit TransferToken(address(this));
+        //return true;
+    }
+
+    function approve(address delegate, uint numTokens) public returns (bool) {
+        allowed[msg.sender][delegate] = numTokens;
+        emit Approval(msg.sender, delegate, numTokens);
+        return true;
+    }
+
+    function allowance(address owner, address delegate) public view returns (uint) {
+        return allowed[owner][delegate];
+    }
+
+    // function transferFrom(address owner, address buyer, uint numTokens) public returns (bool) {
+    //     require(numTokens <= balances[owner]);    
+    //     require(numTokens <= allowed[owner][msg.sender]);
+    
+    //     balances[owner] = balances[owner].sub(numTokens);
+    //     allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens);
+    //     balances[buyer] = balances[buyer].add(numTokens);
+    //     emit Transfer(owner, buyer, numTokens);
+        
+    //     return true;
+    // }
+
+
+    //Internal Functions
+
     function createCampaign(uint amountRequired, address payable vendor) public payable validCreateCampaign(amountRequired) {
         counter = counter + 1;
         Campaign storage campaign = campaigns[counter];
@@ -73,19 +150,19 @@ contract Donations {
         emit CreateCampaign(campaign.id,campaign.target,campaign.deposit,campaign.createdBy,campaign.vendor);
     }
 
-    function donate(uint campaignId) public payable validTransition(campaignId, Status.OPEN) validDonation(campaignId) {
-        Campaign storage campaign = campaigns[campaignId];
-        campaign.fundsRaised += msg.value;
+    // function donate(uint campaignId) public payable validTransition(campaignId, Status.OPEN) validDonation(campaignId) {
+    //     Campaign storage campaign = campaigns[campaignId];
+    //     campaign.fundsRaised += msg.value;
 
-        campaign.donationsHash = uint(keccak256(abi.encodePacked(campaign.donationsHash,msg.value,msg.sender)));
-        emit Donation(campaignId, msg.sender, msg.value);
+    //     campaign.donationsHash = uint(keccak256(abi.encodePacked(campaign.donationsHash,msg.value,msg.sender)));
+    //     emit Donation(campaignId, msg.sender, msg.value);
 
-        if (campaign.fundsRaised == campaign.target){
-            campaign.vendor.transfer(campaign.target);
-            payable(campaign.createdBy).transfer(campaign.deposit);
-            closeCampaign(campaign,Status.CLOSED);
-        }
-    }
+    //     if (campaign.fundsRaised == campaign.target){
+    //         campaign.vendor.transfer(campaign.target);
+    //         payable(campaign.createdBy).transfer(campaign.deposit);
+    //         closeCampaign(campaign,Status.CLOSED);
+    //     }
+    // }
 
     function expireCampaign(uint campaignId, Donor[] calldata donors) public validTransition(campaignId,Status.OPEN) validExpireCampaign(campaignId,donors) {
         Campaign storage campaign = campaigns[campaignId];
@@ -99,5 +176,18 @@ contract Donations {
     function closeCampaign(Campaign storage campaign, Status status) private {
         campaign.status = status;
         emit Transition(campaign.id, status);
+    }
+}
+
+library SafeMath { 
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+      assert(b <= a);
+      return a - b;
+    }
+    
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+      uint256 c = a + b;
+      assert(c >= a);
+      return c;
     }
 }
