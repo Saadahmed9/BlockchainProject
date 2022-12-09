1080dc1e97e5424c4e3a2d36e2dc6b1726b66efd
Sai Kiran Janga <jangasaikiran@gmail.com>
2022-12-08 19:31:58 -0500

    Contract changes

--- donation-contract/contracts/Donations.sol
+++ donation-contract/contracts/Donations.sol
@@ -3,10 +3,12 @@ pragma solidity 0.8.17;
 
 contract Donations {
     
-    enum Status {OPEN, CLOSED, EXPIRED}
-    string public constant name = "MyToken";
+    string public constant name = "DonateCoin";
     string public constant symbol = "DTC";
-    uint8 public constant decimals = 0;  
+    uint8 public constant decimals = 2;  
+
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    event Transfer(address indexed from, address indexed to, uint tokens);
 
     mapping(address => uint256) balances;
 
@@ -15,6 +17,46 @@ contract Donations {
     uint256 totalSupply_;
 
     using SafeMath for uint256;
+
+
+    function totalSupply() public view returns (uint256) {
+	return totalSupply_;
+    }
+    
+    function balanceOf(address tokenOwner) public view returns (uint) {
+        return balances[tokenOwner];
+    }
+
+    function transfer(address receiver, uint numTokens) public returns (bool) {
+        require(numTokens <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender].sub(numTokens);
+        balances[receiver] = balances[receiver].add(numTokens);
+        emit Transfer(msg.sender, receiver, numTokens);
+        return true;
+    }
+
+    function approve(address delegate, uint numTokens) public returns (bool) {
+        allowed[msg.sender][delegate] = numTokens;
+        emit Approval(msg.sender, delegate, numTokens);
+        return true;
+    }
+
+    function allowance(address owner, address delegate) public view returns (uint) {
+        return allowed[owner][delegate];
+    }
+
+    function transferFrom(address owner, address buyer, uint numTokens) public returns (bool) {
+        require(numTokens <= balances[owner]);    
+        require(numTokens <= allowed[owner][msg.sender]);
+    
+        balances[owner] = balances[owner].sub(numTokens);
+        allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens);
+        balances[buyer] = balances[buyer].add(numTokens);
+        emit Transfer(owner, buyer, numTokens);
+        return true;
+    }
+
+    enum Status {OPEN, CLOSED, EXPIRED}
 
     struct Campaign {
         uint id;
@@ -35,22 +77,16 @@ contract Donations {
     mapping(uint => Campaign) public campaigns;
     uint public counter;
     uint public percentToDeposit;
-    //address receiver;
+
     constructor (uint256 total) {
         counter = 0;
         totalSupply_ = total;
 	    balances[address(this)] = totalSupply_;
     }  
 
-    //event Donation(uint campaignId, address _from, uint _value);
     event Transition(uint campaignId, Status status);
     event CreateCampaign(uint campaignId, uint target, uint deposit, address createdBy, address vendor);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
     event Donation(uint campaignId,address from, uint numTokens);
-    event TransferToken(address to_);
-    event GetBalance(uint numTokens);
-    //event Transfer(address indexed from, address indexed to, uint tokens);
-    
 
     modifier validTransition(uint campaignId, Status status) {
         require(campaigns[campaignId].status == status);
@@ -77,52 +113,36 @@ contract Donations {
         _;
     }
 
-    //ERC20 functions
-     function totalSupply() public view returns (uint256) {
-	return totalSupply_;
+    function getBalance() public payable {
+        uint numTokens=20;
+        transferFromContract(msg.sender,numTokens);
     }
-    
-    function DtcBalance(address account) public view returns (uint){
-        return balances[account];
+
+    function transferFromContract(address receiver, uint numTokens) public payable {
+        require(numTokens <= balances[address(this)]);
+        balances[receiver] = balances[receiver].add(numTokens);
+        balances[address(this)]=balances[address(this)].sub(numTokens);
+    }
+
+    function transferToContract(uint numTokens) public payable {
+        require(numTokens <= balances[msg.sender]);
+        balances[address(this)] = balances[address(this)].add(numTokens);
+        balances[msg.sender]=balances[msg.sender].sub(numTokens);
     }
 
     function donate(uint campaignId, uint numTokens) public payable {
         Campaign storage campaign = campaigns[campaignId];
         require((campaign.fundsRaised+numTokens) <= campaign.target);
         campaign.fundsRaised += numTokens;
-        balances[msg.sender] = balances[msg.sender].sub(numTokens);
+        transferToContract(numTokens);
         campaign.donationsHash = uint(keccak256(abi.encodePacked(campaign.donationsHash,numTokens+1,msg.sender)));
         emit Donation(campaignId,msg.sender, numTokens);
         if (campaign.fundsRaised == campaign.target){
-            transfertokentovendor(campaign.vendor,campaign.target);
-            transfertokentovendor(campaign.createdBy,campaign.deposit);
+            transferFromContract(campaign.vendor,campaign.target);
+            transferFromContract(campaign.createdBy,campaign.deposit);
             closeCampaign(campaign,Status.CLOSED);
         }
     }
-
-    function transfertoken() public payable {
-        uint numTokens=20;
-        require(numTokens >= balances[msg.sender]);
-        balances[msg.sender] = balances[msg.sender].add(numTokens);
-        balances[address(this)]=balances[address(this)]-numTokens;
-        emit TransferToken(address(this));
-    }
-    function transfertokentovendor(address account,uint amount) public payable {
-        balances[account] = balances[account].add(amount);
-    }
-
-    function approve(address delegate, uint numTokens) public returns (bool) {
-        allowed[msg.sender][delegate] = numTokens;
-        emit Approval(msg.sender, delegate, numTokens);
-        return true;
-    }
-
-    function allowance(address owner, address delegate) public view returns (uint) {
-        return allowed[owner][delegate];
-    }
-
-
-    //Internal Functions
 
     function createCampaign(uint amountRequired, address payable vendor) public payable {
         uint deposit1=2;
@@ -137,7 +157,8 @@ contract Donations {
         campaign.deposit = deposit1;
         campaigns[counter] = campaign;
         //deposit1=(percentToDeposit*amountRequired)/100;
-        balances[msg.sender] = balances[msg.sender].sub(deposit1);
+        transferToContract(deposit1);
+        // balances[msg.sender] = balances[msg.sender].sub(deposit1);
         emit CreateCampaign(campaign.id,campaign.target,campaign.deposit,campaign.createdBy,campaign.vendor);
     }
 
@@ -145,9 +166,9 @@ contract Donations {
     function expireCampaign(uint campaignId, Donor[] calldata donors) public validTransition(campaignId,Status.OPEN) {
         Campaign storage campaign = campaigns[campaignId];
         for (uint i=0;i<donors.length;i++){
-            payable(donors[i].donorAddress).transfer(donors[i].donatedAmount);
+            transferFromContract(donors[i].donorAddress,donors[i].donatedAmount);
         }
-        payable(campaign.createdBy).transfer(campaign.deposit);
+        transferFromContract(campaign.createdBy,campaign.deposit);
         closeCampaign(campaign, Status.EXPIRED);
     }
 
